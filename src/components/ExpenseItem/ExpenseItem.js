0bc615b6a742905e6e0f9308c8ca3676cf95bb16
Bryant Richards <dragonseyeimages@gmail.com>
2023-03-20 07:19:16 -0500

    adds css

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 // css
 import './style.css'
 
 const ExpenseItem = () => {
   return (
-    <h1>Hello Expense Item</h1>
+    <Fragment>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </Fragment>
   )
 }
 

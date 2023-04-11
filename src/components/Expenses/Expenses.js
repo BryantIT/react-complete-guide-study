00ea550b2ff9fc104277a670346e9f9d1f2ecc44
Bryant Richards <dragonseyeimages@gmail.com>
2023-04-11 07:11:52 -0500

    adds a conditional render if filter is 0

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,28 +9,36 @@ import './style.css'
 const Expenses = ({ expenses }) => {
   const [selectedYear, setSelectedYear] = useState()
   const [displayedExpenses, setDisplayedExpenses] = useState(expenses)
-
+  const [shouldDisplayList, setShouldDisplayList] = useState(true)
 
   const filterExpenses = (year) => {
     if (isNaN(year)) {
       setDisplayedExpenses(expenses)
       return
     }
-    const filtered = expenses.filter(expense => expense.date.getFullYear() === year)
+    const filtered = expenses.filter(
+      (expense) => expense.date.getFullYear() === year
+    )
+    if (filtered.length === 0) {
+      setShouldDisplayList(false)
+      return
+    }
+    setShouldDisplayList(true)
     setDisplayedExpenses(filtered)
   }
 
   return (
     <Fragment>
       <Card className='expenses'>
-      <ExpenseFilter  filterExpenses={filterExpenses}/>
-      {displayedExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          expense={expense}
-        />
-      ))}
-    </Card>
+        <ExpenseFilter filterExpenses={filterExpenses} />
+        {shouldDisplayList ? (
+          displayedExpenses.map((expense) => (
+            <ExpenseItem key={expense.id} expense={expense} />
+          ))
+        ) : (
+          <p className='info'>No Expenses To Show</p>
+        )}
+      </Card>
     </Fragment>
   )
 }
